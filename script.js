6f3f67284ce0eb4cee06d1053f7390095a0faa13
ducmanh02 <89408872+ducmanh02@users.noreply.github.com>
2023-07-07 21:39:08 +0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Script for navigation bar
+
+const mobile_bar = document.getElementById("mobile_bar");
+const header_navbar = document.getElementById("header_navbar");
+const close_header_navbar = document.getElementById("close_header_navbar");
+
+console.log( mobile_bar);
+if(mobile_bar ) {
+    mobile_bar.addEventListener("click", () =>{
+        header_navbar.style.right = "0";
+        
+    });
+}
+if(close_header_navbar) {
+    close_header_navbar.addEventListener("click", () =>{
+        header_navbar.style.right = "-300px";
+       
+    });
+}
